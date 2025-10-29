12c3914374dcb005a41675ba7a3a94cb8df768ff
loohelin-ux <loohelin@gmail.com>
2025-10-29 20:25:56 +0800

    Create quick_start.py

---
+++ examples/quick_start.py
@@ -0,0 +1,5 @@
+Python
+
+# M2RA-DrugAI Framework: Quick Start Example
+# This script demonstrates the conceptual workflow of the M2RA-DrugAI package.
+print("Conceptual workflow script for M2RA-DrugAI.")
